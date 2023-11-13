a6736997cb7a2e3a2954752d1b127e0bebb3336a
Bellwin98 <jongseunghan@naver.com>
2023-11-13 18:27:58 +0900

    Refactor(EventPlanner): 할인 혜택을 확인하고 적용하는 로직을 EventCalendar 클래스에서 처리

--- src/main/java/christmas/domain/EventPlanner.java
+++ src/main/java/christmas/domain/EventPlanner.java
@@ -3,20 +3,19 @@ package christmas.domain;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import static christmas.domain.Discount.*;
-
 public class EventPlanner {
+    private final EventCalendar eventCalendar = new EventCalendar();
     public EventPlanner(){}
 
     public void run (){
         Order order = inputReservationInfo();
-        if (order.isSumOverMinimumAmount()){
-            Map<String, Integer> discountInfo = findDiscountInfo(order);
-            Event event = new Event(discountInfo);
-//            event.printDiscountInfo();
+        if (order.isSumOverMinimumOrderAmount()){
+            List<String> events = eventCalendar.findEvent(order.getReservationDate());
+            order.applyBenefits(events);
+            OutputView.printBenefits(order);
         }
         OutputView.printBenefits(order);
     }
@@ -43,18 +42,5 @@ public class EventPlanner {
             OutputView.printErrorMessage(exception.getMessage());
             return inputOrder();
         }
-    }
-
-    public Map<String, Integer> findDiscountInfo(Order order){
-        Map<String, Integer> discountInfo = new HashMap<>();
-        if (order.isDateForChristmasEvent()){
-            discountInfo.put(CHRISTMAS_D_DAY_DISCOUNT.getContent(), CHRISTMAS_D_DAY_DISCOUNT.getDiscount());
-        }
-        if (order.findDayOfWeek() >= 1 && order.findDayOfWeek() <= 4 || order.findDayOfWeek() == 7){
-            discountInfo.put(WEEKDAY_DISCOUNT.getContent(), WEEKDAY_DISCOUNT.getDiscount());
-            return discountInfo;
-        }
-        discountInfo.put(WEEKEND_DISCOUNT.getContent(), WEEKEND_DISCOUNT.getDiscount());
-        return discountInfo;
     }
 }
