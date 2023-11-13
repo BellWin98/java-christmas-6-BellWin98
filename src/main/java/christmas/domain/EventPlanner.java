22b93d9a39eea0c2fc2742939c27f747892adbc7
Bellwin98 <jongseunghan@naver.com>
2023-11-13 10:01:34 +0900

    Feat: 할인정보를 확인하는 기능 구현

--- src/main/java/christmas/domain/EventPlanner.java
+++ src/main/java/christmas/domain/EventPlanner.java
@@ -3,14 +3,22 @@ package christmas.domain;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import static christmas.domain.Discount.*;
 
 public class EventPlanner {
     public EventPlanner(){}
 
     public void run (){
         Order order = inputReservationInfo();
-        order.printOrderSheet();
+        if (order.isSumOverMinimumAmount()){
+            Map<String, Integer> discountInfo = findDiscountInfo(order);
+            Event event = new Event(discountInfo);
+//            event.printDiscountInfo();
+        }
+        OutputView.printBenefits(order);
     }
 
     public Order inputReservationInfo(){
@@ -35,5 +43,18 @@ public class EventPlanner {
             OutputView.printErrorMessage(exception.getMessage());
             return inputOrder();
         }
+    }
+
+    public Map<String, Integer> findDiscountInfo(Order order){
+        Map<String, Integer> discountInfo = new HashMap<>();
+        if (order.isDateForChristmasEvent()){
+            discountInfo.put(CHRISTMAS_D_DAY_DISCOUNT.getContent(), CHRISTMAS_D_DAY_DISCOUNT.getDiscount());
+        }
+        if (order.findDayOfWeek() >= 1 && order.findDayOfWeek() <= 4 || order.findDayOfWeek() == 7){
+            discountInfo.put(WEEKDAY_DISCOUNT.getContent(), WEEKDAY_DISCOUNT.getDiscount());
+            return discountInfo;
+        }
+        discountInfo.put(WEEKEND_DISCOUNT.getContent(), WEEKEND_DISCOUNT.getDiscount());
+        return discountInfo;
     }
 }
