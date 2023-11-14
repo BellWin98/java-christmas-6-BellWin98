3ee09a13cf1859f20df6331047dc4e17962fe4c5
Bellwin98 <jongseunghan@naver.com>
2023-11-15 00:46:09 +0900

    Refactor(Order): 람다식 및 함수형 프로그래밍 적용, 접근제어자 및 변수명 변경

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -7,27 +7,68 @@ import java.util.Map;
 import static christmas.domain.Discount.*;
 
 public class Order {
-    private static final int MINIMUM_ORDER_AMOUNT = 10000;
-    private static final int ORDER_AMOUNT_FOR_GIFT = 120000;
+    private static final int MINIMUM_ORDER_AMOUNT = 10_000;
+    private static final int ORDER_AMOUNT_FOR_GIFT = 120_000;
     private static final int DISCOUNT_AMOUNT_INCREASE = 100;
     private final int reservationDate;
-    private final int totalOrderAmountBeforeDiscount;
     private final Map<String, Integer> order;
-    private final Map<String, Integer> benefitsInfo;
-    private int totalOrderAmountAfterDiscount;
+    private final Map<String, Integer> benefitsInfo = new HashMap<>();
+    private final int orderAmount; // 주문 금액
+    private int totalAmount; // 총 결제금액
 
     public Order(int reservationDate, Map<String, Integer> order) {
         this.reservationDate = reservationDate;
         this.order = order;
-        totalOrderAmountBeforeDiscount = calculateOrderAmountBeforeDiscount();
-        totalOrderAmountAfterDiscount = totalOrderAmountBeforeDiscount;
-        benefitsInfo = new HashMap<>();
+        orderAmount = calculateOrderAmount();
+        totalAmount = orderAmount;
     }
 
     public void applyBenefits(List<String> events) {
         if (canGetGift()) {
             benefitsInfo.put(GIFT.getContent(), GIFT.getAmount());
         }
+        applyDiscount(events);
+    }
+
+    public boolean isSumOverMinimumOrderAmount() {
+        return order.entrySet()
+                .stream()
+                .mapToInt(orderSheet -> Menu.calculateSumOfMenuPrice(orderSheet.getKey(), orderSheet.getValue()))
+                .sum() > MINIMUM_ORDER_AMOUNT;
+    }
+
+    public boolean canGetGift() {
+        return orderAmount > ORDER_AMOUNT_FOR_GIFT;
+    }
+
+    public int calculateTotalDiscount() {
+        return benefitsInfo.values()
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public Map<String, Integer> getOrder() {
+        return order;
+    }
+
+    public int getReservationDate() {
+        return reservationDate;
+    }
+
+    public Map<String, Integer> getBenefitsInfo() {
+        return benefitsInfo;
+    }
+
+    public int getOrderAmount() {
+        return orderAmount;
+    }
+
+    public int getTotalAmount() {
+        return totalAmount;
+    }
+
+    private void applyDiscount(List<String> events) {
         for (String event : events) {
             if (event.equals(CHRISTMAS_D_DAY.getContent())) {
                 benefitsInfo.put(CHRISTMAS_D_DAY.getContent(), getChristmasD_DayDiscount(CHRISTMAS_D_DAY.getAmount()));
@@ -43,86 +84,43 @@ public class Order {
         benefitsInfo.put(WEEKEND.getContent(), getWeekEndDiscount(WEEKEND.getAmount()));
     }
 
-    public int getChristmasD_DayDiscount(int discountAmount) {
+    private int getChristmasD_DayDiscount(int discountAmount) {
         discountAmount = discountAmount + DISCOUNT_AMOUNT_INCREASE * (reservationDate - 1);
-        totalOrderAmountAfterDiscount -= discountAmount;
+        totalAmount -= discountAmount;
         return discountAmount;
     }
 
-    public int getSpecialDiscount(int discountAmount) {
-        totalOrderAmountAfterDiscount -= discountAmount;
+    private int getSpecialDiscount(int discountAmount) {
+        totalAmount -= discountAmount;
         return discountAmount;
     }
 
-    public int getWeekDayDiscount(int discountAmount) {
-        int count = 0;
-        for (Map.Entry<String, Integer> menu : order.entrySet()) {
-            if (Menu.isMenuCategoryDessert(menu.getKey())) {
-                count += menu.getValue();
-            }
-        }
-        discountAmount = discountAmount * count;
-        totalOrderAmountAfterDiscount -= discountAmount;
-        return discountAmount;
+    private int getWeekDayDiscount(int discountAmount) {
+        int count = order.entrySet()
+                .stream()
+                .filter(menu -> Menu.isMenuCategoryDessert(menu.getKey()))
+                .mapToInt(Map.Entry::getValue)
+                .sum();
+        int calculatedDiscount = discountAmount * count;
+        totalAmount -= calculatedDiscount;
+        return calculatedDiscount;
     }
 
-    public int getWeekEndDiscount(int discountAmount) {
-        int count = 0;
-        for (Map.Entry<String, Integer> menu : order.entrySet()) {
-            if (Menu.isMenuCategoryMain(menu.getKey())) {
-                count += menu.getValue();
-            }
-        }
-        discountAmount = discountAmount * count;
-        totalOrderAmountAfterDiscount -= discountAmount;
-        return discountAmount;
+    private int getWeekEndDiscount(int discountAmount) {
+        int count = order.entrySet()
+                .stream()
+                .filter(menu -> Menu.isMenuCategoryMain(menu.getKey()))
+                .mapToInt(Map.Entry::getValue)
+                .sum();
+        int calculatedDiscount = discountAmount * count;
+        totalAmount -= calculatedDiscount;
+        return calculatedDiscount;
     }
 
-    public boolean isSumOverMinimumOrderAmount() {
-        int sum = 0;
-        for (Map.Entry<String, Integer> orderSheet : order.entrySet()) {
-            sum += Menu.calculateSumOfPrice(orderSheet.getKey(), orderSheet.getValue());
-        }
-        return (sum > MINIMUM_ORDER_AMOUNT);
-    }
-
-    public boolean canGetGift() {
-        return totalOrderAmountBeforeDiscount > ORDER_AMOUNT_FOR_GIFT;
-    }
-
-    public int calculateOrderAmountBeforeDiscount() {
-        int sum = 0;
-        for (Map.Entry<String, Integer> orderSheet : order.entrySet()) {
-            sum += Menu.calculateSumOfPrice(orderSheet.getKey(), orderSheet.getValue());
-        }
-        return sum;
-    }
-
-    public int calculateTotalBenefitAmount() {
-        int sum = 0;
-        for (Integer discountAmount : benefitsInfo.values()) {
-            sum += discountAmount;
-        }
-        return sum;
-    }
-
-    public Map<String, Integer> getOrder() {
-        return order;
-    }
-
-    public int getReservationDate() {
-        return reservationDate;
-    }
-
-    public Map<String, Integer> getBenefitsInfo() {
-        return benefitsInfo;
-    }
-
-    public int getTotalOrderAmountBeforeDiscount() {
-        return totalOrderAmountBeforeDiscount;
-    }
-
-    public int getTotalOrderAmountAfterDiscount() {
-        return totalOrderAmountAfterDiscount;
+    private int calculateOrderAmount() {
+        return order.entrySet()
+                .stream()
+                .mapToInt(orderSheet -> Menu.calculateSumOfMenuPrice(orderSheet.getKey(), orderSheet.getValue()))
+                .sum();
     }
 }
