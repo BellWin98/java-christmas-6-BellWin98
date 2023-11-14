25081178ce02351d3a04a6036045f72457813818
Bellwin98 <jongseunghan@naver.com>
2023-11-15 00:40:42 +0900

    Style(Discount): 금액 형식 수정

--- src/main/java/christmas/domain/Discount.java
+++ src/main/java/christmas/domain/Discount.java
@@ -1,11 +1,11 @@
 package christmas.domain;
 
 public enum Discount {
-    CHRISTMAS_D_DAY("크리스마스 디데이 할인", 1000),
-    WEEKDAY("평일 할인", 2023),
-    WEEKEND("주말 할인", 2023),
-    SPECIAL("특별 할인", 1000),
-    GIFT("증정 이벤트", 25000);
+    CHRISTMAS_D_DAY("크리스마스 디데이 할인", 1_000),
+    WEEKDAY("평일 할인", 2_023),
+    WEEKEND("주말 할인", 2_023),
+    SPECIAL("특별 할인", 1_000),
+    GIFT("증정 이벤트", 25_000);
 
     private final String content;
     private final int amount;
