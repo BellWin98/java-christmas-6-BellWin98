c5f28b66821ca48b5acc6ea1d7d8f763f8a94780
Bellwin98 <jongseunghan@naver.com>
2023-11-15 00:51:52 +0900

    Test: OrderTest 추가

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -1,2 +1,18 @@
-package christmas.domain;public class OrderTest {
+package christmas.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OrderTest {
+
+    @Test
+    @DisplayName("주말에는 주말 할인이 적용된다")
+    void givenWeekendDate_ThenApplyWeekendDiscount(){
+        // given
+
+
+        // when
+
+        // then
+    }
 }
