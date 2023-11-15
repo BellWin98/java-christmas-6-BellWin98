bf053c47fdb246726858b155f81ff4ac33945c03
Bellwin98 <jongseunghan@naver.com>
2023-11-15 21:15:30 +0900

    Test: 메뉴 개수 20개 초과 시 예외 발생하는 테스트 코드 작성

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -1,18 +1,30 @@
 package christmas.domain;
 
+import christmas.view.InputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static christmas.exception.ChristmasException.MENU_COUNT_OVER_TWENTY_ERROR;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class OrderTest {
 
     @Test
-    @DisplayName("주말에는 주말 할인이 적용된다")
-    void givenWeekendDate_ThenApplyWeekendDiscount(){
-        // given
+    @DisplayName("메뉴의 개수가 20개를 초과하면 예외가 발생한다.")
+    void givenMenusOverTwenty_ThenFail(){
 
+        Map<String, Integer> order = new HashMap<>();
+        order.put("해산물파스타", 10);
+        order.put("티본스테이크", 15);
 
-        // when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            InputView.validateMenuCountOverTwenty(order);
+        });
 
-        // then
+        assertEquals(MENU_COUNT_OVER_TWENTY_ERROR.getMessage(), exception.getMessage());
     }
 }
